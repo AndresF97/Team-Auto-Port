9abd8bba3660c1f39574711fe2efe8f486d37cf3
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-08 21:33:18 -0800

    finnished running succesful test and working on app.js prompt

--- app.js
+++ app.js
@@ -1,63 +1,8 @@
 const inquirer = require("inquirer");
-
-promptManager = function() { 
+const promptManager = function(){
     return inquirer.prompt([
-    {
-        input:"input",
-        message:"Whats your ID?",
-        name:"ID",
-    },{
-        input:"input",
-        message:"Whats your Email?",
-        name:"email"
-    },{
-        input:"input",
-        message:"Whats your office Number?",
-        name:"officeNum"
-    }
-
-    ])
-}
-promptIntern = function() { 
-    return inquirer.prompt([
-    {
-        input:"input",
-        message:"Whats your ID?",
-        name:"ID",
-    },{
-        input:"input",
-        message:"Whats your Email?",
-        name:"email"
-    },{
-        input:"input",
-        message:"Whats the name of the school graduated from?",
-        name:"officeNum"
-    }
-
-    ])
-}
-
-
-promptUser = function(){
-        return inquirer.prompt([
-        {
-            type:"input",
-            message:"Whats your name?",
-            name:"name"
-        },{
-            type:"input",
-            message:"Whats your position?",
-            name:"position"
-        },
         
     ])
-}
-promptUser().then(function({name,position}){
-   if(position === "Manager" || position ==="manager"){
-       return promptManager()
-   }
-   if(position === "Intern" || position ==="intern"){
-    return promptIntern()
-}
 
-})
+}
+promptManager()
