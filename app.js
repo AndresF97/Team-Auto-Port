41cbea1f365863cf3b644293b1151342f94f348b
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-10 22:11:46 -0800

    generates html but must work on some small bugs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./lib/Employee");
+const util = require("util");
+const writeToFileASync = util.promisify(fs.writeFile)
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
 const employees = []
+const htmlBody=``;
+let card_manager = ``;
 const promptManager = async function(){
     return inquirer.prompt([
         {
@@ -31,6 +34,19 @@ const promptManager = async function(){
         }
     ]).then(async function({name,id,email,office,choice}){
         const manager = new Manager(name,id,email,office)
+        card_manager =`
+        <div class="card m-3" style="width: 18rem;">
+        <div class="card-header bg-primary text-white">
+          <h1>${manager.name}</h1>
+          <h2><i class="fas fa-glasses"></i> &nbsp;Manager</h2>
+        </div>
+        <div class="container bg-light">
+              <ul class="list-group p-3">
+                  <li class="list-group-item">ID :${manager.id}</li>
+                  <li class="list-group-item">Email : <a href="https://mail.google.com/"class="card-link">${manager.email}</a> </li>
+                  <li class="list-group-item">Office number: ${manager.office}</li>
+              </ul>
+              `
         employees.push(manager)
         console.log(employees)
         console.log(("-").repeat())
@@ -69,8 +85,20 @@ const promptEngineer = function(){
         }
     ]).then( async function({name,id,email,github,choice}){
         const engineer = new Engineer(name,id,email,github)
+        const engineer_card = `<div class="card m-3" style="width: 18rem;">
+        <div class="card-header bg-primary text-white">
+          <h1>${engineer.name}</h1>
+          <h2><i class="fas fa-laptop-code"></i> &nbsp;Engineer</h2>
+        </div>
+        <div class="container bg-light">
+              <ul class="list-group p-3">
+                  <li class="list-group-item">ID : ${engineer.id}</li>
+                  <li class="list-group-item">Email : <a href="https://mail.google.com/"class="card-link">${engineer.email}</a> </li>
+                  <li class="list-group-item">Github account: <a href="https://github.com/"class="card-link">${engineer.github}</a> </li>
+              </ul>
+              `
+
         employees.push(engineer)
-        console.log(employees)
         console.log(("-").repeat())
             if(choice === "Engineer"){
                 return promptEngineer()
@@ -107,7 +135,18 @@ const promptIntern = function(){
     ]).then(async function({name,id,email,github,choice}){
         const intern = new Intern(name,id,email,github)
         employees.push(intern)
-        console.log(employees)
+        const intern_card  =`<div class="card m-3" style="width: 18rem;">
+        <div class="card-header bg-primary text-white">
+          <h1>${intern.name}</h1>
+          <h2><i class="fas fa-laptop-code"></i> &nbsp;Intern</h2>
+        </div>
+        <div class="container bg-light">
+              <ul class="list-group p-3">
+                  <li class="list-group-item">ID : ${intern.id}</li>
+                  <li class="list-group-item">Email : <a href="https://mail.google.com/"class="card-link">${engineer.email}</a> </li>
+                  <li class="list-group-item">School: <a href="https://www.google.com/search?sxsrf=ACYBGNTZXX802ACAWNWCBEgK0prDTsDYqA%3A1578721229634&source=hp&ei=zV8ZXvenJM-4-gS0xLbABQ&q=${intern.school}s&oq=${intern.school}&gs_l=psy-ab.3..0i131j0l3j0i131j0j0i131j0j0i131j0.2096.3148..3471...2.0..0.80.383.5......0....1..gws-wiz.....10..35i362i39j35i39.6UWdr0Tunp4&ved=0ahUKEwi3wtvw6vrmAhVPnJ4KHTSiDVgQ4dUDCAg&uact=5"class="card-link">${engineer.github}</a> </li>
+              </ul>
+              `
         console.log(("-").repeat())
             if(choice === "Engineer"){
                 return promptEngineer()
@@ -118,5 +157,27 @@ const promptIntern = function(){
          })
 
 }
+writeFile = (data) =>{
+    writeToFileASync("./output/team.html",data)
+}
 console.log("Please Build your team!")
-promptManager()
+ promptManager().then(function(err){
+     return generatehtml()
+    
+})
+
+
+// var htmlfoot = ``
+function generatehtml(){
+    var htmlHead = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+                <title>Your A-Team!</title>
+            </head>`+htmlBody;
+    writeToFileASync("./output/index.html",htmlBody);
+
+}
